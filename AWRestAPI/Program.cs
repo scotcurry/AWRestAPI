fa957f71be674b9b4d76b9838a643039955e60d8
ScotCurry <scotcurry4@gmail.com>
2016-02-24 21:01:56 -0600

    Initial attempts to make this a solid test for REST API's against the AirWatch / VMware APIs

--- AWRestAPI/Program.cs
+++ AWRestAPI/Program.cs
@@ -4,12 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* This is not really a program.  This is a test harness for the RestHandler.cs class.  To get anything useful out of
+   this you will need to populate some variables in the [TestInitialize] portion of the the AWRestAPITests project.
+*/
 namespace AWRestAPI
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("This is a class creation project.  Run the test to make sure the class is working");
+            Console.ReadLine();
         }
     }
 }
